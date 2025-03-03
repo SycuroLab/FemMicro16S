6e1315f9717f45763003340eebc2b4fe72ede8d0
saharbagheri <sahar.bagheri1@ucalgary.ca>
2025-03-03 00:42:31 -0700

    Update removeChimeras.R

--- utils/scripts/dada2/removeChimeras.R
+++ utils/scripts/dada2/removeChimeras.R
@@ -7,6 +7,16 @@ seqtab.all= readRDS(snakemake@input[['seqtab']]) # seqtab.all
 # Remove chimeras
 seqtab <- removeBimeraDenovo(seqtab.all, method=snakemake@config[['chimera_method']], multithread=snakemake@threads)
 
+A <- sum(seqtab) / sum(seqtab.all)
+
+removed_percent <- (1 - A) * 100
+
+message <- paste0(
+  "Abundances of removed chimeric sequence variants was ",
+  round(removed_percent, 2), "% of the merged sequence reads"
+)
+print(message)
+
 saveRDS(seqtab, snakemake@output[['rds']])
 write.csv(seqtab, snakemake@output[['csv']])
 

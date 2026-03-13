29b339efe382043f0b8a6bc2285f97f7d7d35e04
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-03-13 09:57:31 -0600

    removing phix percentage check

--- utils/scripts/dada2/dada2_filter.R
+++ utils/scripts/dada2/dada2_filter.R
@@ -8,17 +8,6 @@ filtFs <- snakemake@output[['R1']]
 filtRs <- snakemake@output[['R2']]
 
 
-track.filt_keep_phix <- filterAndTrim(fnFs,filtFs, 
-                            fnRs,filtRs, 
-#                            truncLen= snakemake@config[["truncLen"]],
-                            maxN=0,
-                            maxEE=snakemake@config[["maxEE"]], 
-                            truncQ=snakemake@config[["truncQ"]],
-                            compress=TRUE,
-                            verbose=TRUE,
-                            multithread=snakemake@threads,
-                            rm.phix=FALSE)
-
 track.filt <- filterAndTrim(fnFs,filtFs, 
                             fnRs,filtRs, 
 #                            truncLen= snakemake@config[["truncLen"]],
@@ -31,7 +20,6 @@ track.filt <- filterAndTrim(fnFs,filtFs,
                             rm.phix=TRUE)
 
 row.names(track.filt) <- snakemake@params[["samples"]]
-row.names(track.filt_keep_phix) <- snakemake@params[["samples"]]
 
 colnames(track.filt) = c('afterCutadapt','filtered')
 
@@ -39,7 +27,3 @@ track.filt<-data.frame(track.filt)
 track.filt <- track.filt[track.filt$filtered > 0, ]
 
 write.table(track.filt,snakemake@params[['nread']],  sep='\t')
-
-percent_phix=data.frame(percent_phix=100*(track.filt_keep_phix[,2]-track.filt[,2])/track.filt[,1])
-
-write.table(percent_phix,snakemake@params[['percent_phix']],  sep='\t')

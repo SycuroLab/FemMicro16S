4b0778b6c8f68ae204019bb2abd732fe95a8a1bc
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-05-21 15:58:51 -0600

    Create dada2_apptainer_sbatch.sh

---
+++ dada2_apptainer_sbatch.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#SBATCH --partition=synergy,cpu2019,cpu2021,cpu2022,cpu2023,cpu2017-bf05,cpu2019-bf05
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-00:00:00
+#SBATCH --mem=38G
+#SBATCH --error=record_dada2.%J.err
+#SBATCH --output=record_dada2.%J.out
+
+log_dir="$(pwd)"
+log_file="logs/dada2-analysis.log.txt"
+num_jobs=60
+
+echo "started at: `date`"
+
+conda activate snakemake
+
+snakemake --unlock
+
+
+#Preparing apptainer envs
+
+mkdir -p apptainer
+
+apptainer pull apptainer/qc-1.0.0.sif library://saharbagheri/femmicro16s/qc:1.0.0
+
+apptainer pull apptainer/dada2-1.0.0.sif library://saharbagheri/femmicro16s/dada2:1.0.0
+
+apptainer pull apptainer/rmd-1.0.0.sif library://saharbagheri/femmicro16s/rmd:1.0.0
+
+apptainer pull apptainer/vsearch-1.0.0.sif library://saharbagheri/femmicro16s/vsearch:1.0.0
+
+
+
+#To run snakemake and singularity
+snakemake --singularity-args "-B /bulk" --rerun-triggers mtime --latency-wait 60 --rerun-incomplete  --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-singularity &>> $log_dir/$log_file
+
+
+
+output_dir=$(grep "output_dir" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
+list_files=$(grep "sampletable" < config.yaml | cut -d ' ' -f2 | sed 's/"//g')
+
+#Copying all snakemake/log files of the run in the output folder
+snakemake_file_dir="${output_dir}/snakemake_files"
+mkdir -p $snakemake_file_dir
+
+cp $list_files $snakemake_file_dir
+cp Snakefile $snakemake_file_dir
+cp config.yaml $snakemake_file_dir
+cp cluster.json $snakemake_file_dir
+cp dada2_sbatch.sh $snakemake_file_dir 
+cp -rf logs $snakemake_file_dir
+cp -rf utils $snakemake_file_dir
+
+bash Version_check.sh > used_tools_versions.txt
+
+echo "Finished at: `date`"

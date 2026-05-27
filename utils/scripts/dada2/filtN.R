a9e2e7c46d6dfa0a44c5f46879d6fc88a7d3943e
saharbagheri <sahar.bagheri1@ucalgary.ca>
2026-05-27 16:14:48 -0600

    Update filtN.R

--- utils/scripts/dada2/filtN.R
+++ utils/scripts/dada2/filtN.R
@@ -14,5 +14,12 @@ fnRs <- snakemake@input[['R2']]
 fnFs.filtN <- snakemake@output[['R1']] # Put N-filtered files in filtN/ subdirectory
 fnRs.filtN <- snakemake@output[['R2']]
 
-out<-filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE)
+out<-filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, rm.phix=FALSE, multithread = TRUE) #This is to calculate phix reads percentage
+
+out1<-filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, rm.phix=TRUE, multithread = TRUE) #These results are going to be used for downstream analysis
+
+
+percent_phix=data.frame(percent_phix=100*(out[,2]-out1[,2])/out[,1])
+
+write.table(percent_phix,snakemake@params[['percent_phix']],  sep='\t')
 
